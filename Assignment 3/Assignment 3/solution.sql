1d89f5942f6053d33f8ac586f6a29e550808ac71
L1F22BSCS1057 <L1F22BSCS1057@ucp.com>
2024-06-05 18:37:26 +0500

    added pdf file for database assignment 3

--- Assignment 3/Assignment 3/solution.sql
+++ Assignment 3/Assignment 3/solution.sql
@@ -1,6 +1,8 @@
--- Active: 1714057803737@@127.0.0.1@3306@assignment3
+-- Active: 1717594420509@@127.0.0.1@3306@assignment3
 use assignment3;
 
 -- task 1
 show tables;
 select * from assignment3.employees;
+
+-- task 1
