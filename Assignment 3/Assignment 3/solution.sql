a28bc2c6c60ca54f7f6c9f17856c72cf6f9f51ab
L1F22BSCS1057 <L1F22BSCS1057@ucp.com>
2024-06-05 19:10:55 +0500

    did some work on solution of assignment 3

--- Assignment 3/Assignment 3/solution.sql
+++ Assignment 3/Assignment 3/solution.sql
@@ -6,3 +6,14 @@ show tables;
 select * from assignment3.employees;
 
 -- task 1
+select `FIRST_NAME`, `LAST_NAME` from employees where `SALARY` > (
+    select `SALARY` from employees where `LAST_NAME` = 'Bull'
+);
+
+-- task 2
+
+select `FIRST_NAME`, `LAST_NAME` from employees where `DEPARTMENT_ID` = (
+    select `DEPARTMENT_ID` from department where `DEPARTMENT_NAME` = 'IT'
+);
+
+-- task 3
